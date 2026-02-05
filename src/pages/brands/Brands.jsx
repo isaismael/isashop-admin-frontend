396f0f23d96efe8009a79f727715ac5b025280ed
Ismael Isa <ismael.isa02@gmail.com>
2026-02-05 17:09:15 -0300

    add page brand

--- src/pages/brands/Brands.jsx
+++ src/pages/brands/Brands.jsx
@@ -1,6 +1,13 @@
 
 export const Brands = () => {
   return (
-    <div>Module Brands</div>
+    <div>
+      {/* header */}
+      <div className="mb-4">
+        <h2 className="text-2xl font-bold">Gestor de marcas</h2>
+        <p className="text-slate-500">Defina información clave de la marca y recursos visualmente enriquecidos para lograr una exhibición atractiva.</p>
+      </div>
+      {/* fin header */}
+    </div>
   )
 }
