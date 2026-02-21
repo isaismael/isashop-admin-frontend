cd983159f9d2c51af87b4d3cc60bc5cd64ad37a8
Ismael Isa <ismael.isa02@gmail.com>
2026-02-21 16:33:20 -0300

    se corrigen services de products

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,42 +1,42 @@
-import api from './api.service';
+import api from "./api.service";
 
-export default class ProductService{
-    constructor(){
-        this.endpoint = '/product/createproduct';
-        this.get_products = "/product/getproducts/"
-        this.updateUrl = "/product/updateproduct/";
-    }
+export default class ProductService {
+  constructor() {
+    this.endpoint = "/product/createproduct";
+    this.get_products = "/product/getproducts/";
+    this.updateUrl = "/product/updateproduct/";
+  }
 
-    async createProduct(productData){
-        try{
-            const response = await api.post(this.endpoint, productData);
-            return response.data;
-        }catch(error){
-            console.log(error.message);
-            return null;
-        }
+  async createProduct(productData) {
+    try {
+      const response = await api.post(this.endpoint, productData);
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+      return null;
     }
+  }
 
-    // -> http://localhost:3014/api/product/getproducts/:page/:limit
-    async getProducts(page, limit){
-        try {
-            const response = await api.get(`${this.get_products}${page}/${limit}`);
-            return response.data;
-        } catch (error) {
-            console.error("Error en getProducts - service: ", error.message);
-            throw error;
-        }
+  async getProducts(page, limit) {
+    try {
+      const response = await api.get(`/product/getproducts`, {
+        params: { page, limit },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error en getProducts - service: ", error.message);
+      throw error;
     }
-    
-    // ->
-    async updateProduct(id, productData){
-        try {
-            const response = await api.put(`${this.updateUrl}${id}`, productData);
-            return response.data;
-        } catch (error) {
-            console.error("Error en updateProduct: ", error.message);
-            throw error;
-        }
-    }
+  }
 
+  // ->
+  async updateProduct(id, productData) {
+    try {
+      const response = await api.put(`${this.updateUrl}${id}`, productData);
+      return response.data;
+    } catch (error) {
+      console.error("Error en updateProduct: ", error.message);
+      throw error;
+    }
+  }
 }
